aa25a41a988509c34c3bc7f683989eeecdd5ebed
MattSux <matthewheilman@outlook.com>
2023-08-25 15:08:12 -0500

    first commit

---
+++ api/User.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+//Sign up
+router.post('/signup', (req, res) => {
+
+
+})
+
+//Sign in
+router.post('/signin', (req, res) => {
+
+
+})
+
+module.exports = router;
